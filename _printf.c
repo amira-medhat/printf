a0017998d8423cd6c077552a877a1461935ab554
AMIRA <amira@AMIRA.myguest.virtualbox.org>
2023-08-26 18:57:38 +0300

    task 1

--- _printf.c
+++ _printf.c
@@ -3,6 +3,77 @@
 #include <unistd.h>
 
 /**
+* _write_char - Writes a character to stdout
+* @c: The character to write
+* @char_counter: Pointer to the character count
+*/
+void _write_char(char c, int *char_counter)
+{
+	write(1, &c, 1);
+	(*char_counter)++;
+}
+
+/**
+* _write_str - Writes a string to stdout
+* @str: The string to write
+* @char_counter: Pointer to the character count
+*/
+void _write_str(char *str, int *char_counter)
+{
+	int str_len = 0;
+
+	while (str && str[str_len])
+	str_len++;
+	if (str == NULL)
+		str = "(null)";
+	write(1, str, str_len);
+	(*char_counter) += str_len;
+}
+
+/**
+* _write_int - Writes an integer to stdout
+* @num: The integer to write
+* @char_counter: Pointer to the character count
+*/
+void _write_int(int num, int *char_counter)
+{
+	int num = va_arg(args_list, int);
+	int num_len = 0;
+	int temp = num;
+
+	if (temp == 0)
+		num_len = 1;
+	else if (temp < 0)
+	{
+		num_len++;
+		temp = -temp;
+	}
+	while (temp != 0)
+	{
+		temp /= 10;
+		num_len++;
+	}
+	char num_str[12];
+	char *num_ptr = num_str + num_len - 1;
+
+	if (num < 0)
+	{
+		num_str[0] = '-';
+		num = -num;
+	}
+
+	while (num > 0)
+	{
+		*num_ptr = num % 10 + '0';
+		num /= 10;
+		num_ptr--;
+	}
+
+	write(1, num_ptr + 1, num_len);
+	char_counter += num_len;
+}
+
+/**
 * _printf - Custom printf function
 * @format: Format string
 *
@@ -22,52 +93,36 @@ int _printf(const char *format, ...)
 
 	while (*format)
 	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			char_counter++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-				break;
+	if (*format != '%')
+	{
+		_write_char(*format, &char_counter);
+	}
+	else
+	{
+		format++;
+		if (*format == '\0')
+			break;
 
-			if (*format == 'c')
-			{
-				char c = va_arg(args_list, int);
-				write(1, &c, 1);
-				char_counter++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args_list, char *);
-				int str_len = 0;
-				while (str && str[str_len])
-					str_len++;
-				if (str == NULL)
-					str = "(null)";
-				write(1, str, str_len);
-				char_counter += str_len;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args_list, int);
-				int num_len = 0;
-				while (num)
-				{
-					num = num / 10;
-					num_len++;
-				}
-				char_counter += num_len;
-				write(1, &num, num_len);
-			}
-			else if (*format == '%')
-			{
-				write(1, format, 1);
-				char_counter++;
-			}
+		if (*format == 'c')
+		{
+			char c = va_arg(args_list, int);
+			_write_char(c, &char_counter);
 		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(args_list, char *);
+			_write_str(str, &char_counter);
+		}
+		else if (*format == 'd' || *format == 'i')
+		{
+			int num = va_arg(args_list, int);
+			_write_int(num, &char_counter);
+		}
+		else if (*format == '%')
+		{
+			_write_char('%', &char_counter);
+		}
+	}
 	format++;
 	}
 	va_end(args_list);
--- main.h
+++ main.h
@@ -9,4 +9,10 @@
 
 int _printf(const char *format, ...);
 
+void _write_char(char c, int *char_counter);
+
+void _write_str(char *str, int *char_counter);
+
+void _write_int(int num, int *char_counter);
+
 #endif /* MAIN_H */
